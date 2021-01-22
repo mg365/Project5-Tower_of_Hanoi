b21230d8a4aa43b595dc9a347d805e8068218571
Mireia Guinovart <m.guinovart.castan@gmail.com>
2021-01-22 11:57:31 +0100

    Hanoi game

---
+++ Hanoi_Towers.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from stack import Stack
+
+print("\nLet's play Towers of Hanoi!!")
+
+#Create the Stacks
+
+stacks = []
+left_stack = Stack('Left')
+middle_stack = Stack('Middle')
+right_stack = Stack('Right')
+
+stacks.append(left_stack)
+stacks.append(middle_stack)
+stacks.append(right_stack)
+
+#Set up the Game
+num_disks = int(input("\nHow many disks do you want to play with?\n"))
+while num_disks < 3:
+  num_disks = int(input("Enter a number greater than or equal to 3\n"))
+
+for i in range(num_disks, 0, -1):
+  left_stack.push(i)
+num_optimal_moves = 2**num_disks - 1
+print(f"\nThe fastest you can solve this game is in {num_optimal_moves} moves")
+
+
+#Get User Input
+def get_input():
+  choices = [i.get_name()[0] for i in stacks]
+  while True:
+    for i in range(len(stacks)):
+      name = stacks[i].get_name()
+      letter = choices[i]
+      print(f"Enter {letter} for {name}")
+    user_input = input('')
+    if user_input in choices:
+      index = choices.index(user_input)
+      return stacks[index]
+    
+
+#Play the Game
+
+num_user_moves = 0
+
+while right_stack.get_size() != num_disks:
+  print("\n\n\n...Current Stacks...")
+  
+  for stack in stacks:
+    stack.print_items()
+  while True:
+    print("\nWhich stack do you want to move from?\n")
+    from_stack = get_input()
+    print("\nWhich stack do you want to move to?\n")
+    to_stack = get_input()
+    if from_stack.get_size() == 0:
+      print('from stack size is 0?',from_stack.get_size)
+      print("\n\nInvalid Move. Try Again")
+    elif to_stack.get_size() == 0 or from_stack.peek()< to_stack.peek():
+      disk = from_stack.pop()
+      to_stack.push(disk)
+      num_user_moves += 1
+      break
+    else:
+      print('smth else happened!','TO: ', to_stack.get_size(),'FROM: ', from_stack.get_size())
+      print("\n\nInvalid Move. Try Again")
+print(f"\n\nYou completed the game in {num_user_moves} moves, and the optimal number of moves is {num_optimal_moves}")
+
+
+# In[ ]:
+
+
+
+
---
+++ node.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+class Node:
+  def __init__(self, value, next_node=None):
+    self.value = value
+    self.next_node = next_node
+    
+  def set_next_node(self, next_node):
+    self.next_node = next_node
+    
+  def get_next_node(self):
+    return self.next_node
+  
+  def get_value(self):
+    return self.value
+
+
+# In[ ]:
+
+
+
+
